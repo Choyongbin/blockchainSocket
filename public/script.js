b0ae63d03ca876a4f5bd2be2fd57e65ccbc7c708
GeonNamGung <rndrjs123@naver.com>
2021-10-03 15:54:54 +0900

    Release v1.0.0

--- public/script.js
+++ public/script.js
@@ -1,56 +1,54 @@
-const socket = io('/')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io("/");
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
 
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
+// const myPeer = new Peer(undefined, {
+//   host: "/",
+//   port: "3001",
+// });
+const myPeer = new Peer();
 
+let peers = [];
 
+socket.on("user-connected", (userId) => {
+  peers.push(userId);
+  //console.log(userId)
+  //messageContainer.append(userId)
+  connectToNewUser(userId);
+});
 
-let peers = {}
+socket.on("userId", (data) => {
+  console.log(data);
+});
 
-socket.on('user-connected', userId => {
-    peers[userId] = userId
-    //console.log(userId)
-    //messageContainer.append(userId)
-    connectToNewUser(userId)
-})
-
-socket.on('userId', data=> {
-  console.log(data)
-})
-
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].delete()
-})
-
-
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-  console.log(id)
-})
-
-function connectToNewUser(userId){
-  for(values in peers){
-    let conn = myPeer.connect(values)
-
-    conn.on('open', function(){
-      console.log("connect " + conn.peer)
-    })
-
-    conn.send('dddddd')
-
-    conn.on('data', function(data){
-      console.log(data)
-    })
-
-    conn.on('close', function(){
-      console.log('close')
-    })
+socket.on("user-disconnected", (userId) => {
+  if (peers.indexOf(userId !== -1)) {
+    peers.splice(peers.indexOf(userId), 1);
   }
+});
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+  console.log(id);
+});
+
+function connectToNewUser(userId) {
+  peers.forEach((peer) => {
+    let conn = myPeer.connect(peer);
+
+    conn.on("open", function () {
+      console.log("connect " + conn.peer);
+    });
+
+    conn.send("dddddd");
+
+    conn.on("data", function (data) {
+      console.log(data);
+    });
+
+    conn.on("close", function () {
+      console.log("close");
+    });
+  });
 }
-
-
--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-const { json } = require('express')
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const { json } = require("express");
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-const users = []
+const PORT = process.env.PORT ?? 3003;
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+const users = [];
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+app.get("/", (req, res) => {
+  console.log("handle");
+  // res.redirect(`/${uuidV4()}`);
+});
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    console.log(userId)
-    socket.join(roomId)
-    users.push(userId)
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-    socket.broadcast.to(roomId).emit('user-connected', userId)
+const currentRoomId = "currentRoom" ?? uuidV4();
+io.on("connect", (socket) => {
+  socket.emit("roomId", currentRoomId);
 
-    socket.broadcast.to(roomId).emit('userId', JSON.stringify(users))
-    console.log(JSON.stringify(users))
+  socket.on("join-room", (roomId, userId) => {
+    console.log("on join-room", roomId, userId);
+    socket.join(roomId);
 
-    socket.on('disconnect', () => {
-      socket.broadcast.to(roomId).emit('user-disconnected', userId)
-    })
-  })
-})
+    //socket broadcast는 '발신자를 제외한'
+    //해당 룸에 존재하는 모든 클라이언트에게 브로드캐스트한다
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+    socket.emit("user-list", JSON.stringify(users));
+    users.push(userId);
 
-server.listen(3000)
+    // socket.broadcast.to(roomId).emit("userId", JSON.stringify(users));
+
+    console.log("users", users);
+
+    socket.on("disconnect", () => {
+      console.log(userId, "is disconnected");
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+      if (users.indexOf(userId) !== -1) {
+        users.splice(users.indexOf(userId), 1);
+      }
+    });
+  });
+});
+
+server.listen(PORT);
