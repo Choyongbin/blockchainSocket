3f44ddade0594473eb4928c5691da22d6833441d
Choyongbin <x7520@naver.com>
2021-12-24 15:35:08 +0900

    blockchain_socket_server_final

--- public/script.js
+++ public/script.js
@@ -1,182 +1,54 @@
-const socket = io('/')
-//const EC = require('elliptic').ec;
-//const ec = new EC('secp256k1');
-//const {BlockChain, Transaction} = require('./blockchain');
-const blockContainer = document.getElementById('message-container')
-const blockForm = document.getElementById('send-container')
-const blockInput = document.getElementById('message-input')
-const blockButton = document.getElementById('send-button')
+const socket = io("/");
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
 
+// const myPeer = new Peer(undefined, {
+//   host: "/",
+//   port: "3001",
+// });
+const myPeer = new Peer();
 
-const txContainer = document.getElementById('message-container2')
-const txForm = document.getElementById('send-container2')
-const txInput = document.getElementById('message-input2')
-const txButton = document.getElementById('send-button2')
+let peers = [];
 
-const myPeer = new Peer(undefined, {
-  host: '3.37.53.134',
-  port: '3001'
-})
+socket.on("user-connected", (userId) => {
+  peers.push(userId);
+  //console.log(userId)
+  //messageContainer.append(userId)
+  connectToNewUser(userId);
+});
 
-let peers = []
-let connections = []
-let myId = null
-// 1. 블록 전부 요청(새로운 유저가 들어 왔을 때)
-// 2. pandding tx 요청(내가 pandding tx 만들어서 나머지에게 보냄)
-// 3. pandding tx 가져오기 요청(새로운 유저가 들어와서 여태까지의 pandding tx 요청)
-// 4. 블록 생성 요청(내가 블록 만들었을 때)
-let messageType = ['request-fullblock', 'make-panddingtx', 'request-panddingtx', 'make-block']
+socket.on("userId", (data) => {
+  console.log(data);
+});
 
-// 새로 유저 들어온 유저 빼고 모두에게 broadcast
-socket.on('user-connected', userId => {
-    peers.push(userId)
-})
-
-//새로 들어온 유저에게만 보내는 통신
-socket.on('userId', data=> {
-  //console.log(data)
-  let temp = JSON.parse(data)
-  for(var i = 0; i<temp.length; i++){
-    peers.push(temp[i])
+socket.on("user-disconnected", (userId) => {
+  if (peers.indexOf(userId !== -1)) {
+    peers.splice(peers.indexOf(userId), 1);
   }
-  if(peers.length >= 2){
-    connectToNetwork()
-  }
-})
+});
 
-//유저가 빠져나갔다고 모두에게 broadcast
-socket.on('user-disconnected', userId => {
-  for(let i = 0; i<peers.length; i++){
-    if(peers[i] === userId){
-      peers.splice(i,1)
-      i--;
-    }
-  }
-})
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+  console.log(id);
+});
 
-//peer 만들기
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
+function connectToNewUser(userId) {
+  peers.forEach((peer) => {
+    let conn = myPeer.connect(peer);
 
-//새로운 peer가 나에게 연결 요청을 보냈을 때
-myPeer.on('connection', function(conn){
-  console.log(conn.peer + ' connect to ' + myPeer.id)
-  connections.push(conn)
+    conn.on("open", function () {
+      console.log("connect " + conn.peer);
+    });
 
-  conn.on('open', function(){
-    conn.on('data', function(data){
-      judgeMessage(data)
-      console.log(data)
-    })
+    conn.send("dddddd");
 
-    conn.send(messageType[0] + ' ' + blockContainer.innerText)
-    conn.send(messageType[2] + ' ' + txContainer.innerText)
+    conn.on("data", function (data) {
+      console.log(data);
+    });
 
-    conn.on('close',function(){
-      for(let i = 0; i<connections.length; i++){
-        if(connections[i] === conn){
-          connections.splice(i,1)
-          i--;
-        }
-      }
-    })
-  })
-})
-
-myPeer.on('close', function(conn){
-  console.log('disconnect')
-  for(let i = 0; i<connections.length; i++){
-    if(connections[i] === conn){
-      connections.splice(i,1)
-      i--;
-    }
-  }
-})
-
-myPeer.on('disconnected', function(conn){
-  console.log(conn)
-  for(let i = 0; i<connections.length; i++){
-    if(connections[i] === conn){
-      connections.splice(i,1)
-      i--;
-    }
-  }
-  myPeer.reconnect();
-})
-
-//block 만들었다고 peer들 한테 보냄
-blockButton.onclick = function(){
-  blockContainer.innerHTML += '<div>'+blockInput.value+'<div>'
-  for(var i = 0; i<connections.length; i++){
-    connections[i].send(messageType[3] + ' ' + blockInput.value) //블록이 들어가면 되는 자리
-  }
-  blockInput.value=''
+    conn.on("close", function () {
+      console.log("close");
+    });
+  });
 }
-
-//tx 만들었다고 peer들 한테 보냄
-txButton.onclick = function(){
-  txContainer.innerHTML += '<div>'+txInput.value+'<div>'
-  for(var i = 0; i<connections.length; i++){
-    connections[i].send(messageType[1] + ' ' +txInput.value)  //tx가 들어가면 되는 자리
-  }
-  txInput.value=''
-}
-
-//새로 들어온 peer가 다른 모두에게 연결 보내는 통신
-function connectToNetwork(){
-  for(var i = 0; i< peers.length -1 ; i++){
-    let conn = myPeer.connect(peers[i])
-
-    conn.on('open', function(){
-      connections.push(conn)
-      console.log(conn.peer + ' connect to ' + myPeer.id)
-
-      conn.on('data', function(data){
-        judgeMessage(data)
-      })
-
-      conn.on('close',function(){
-        for(let i = 0; i<connections.length; i++){
-          if(connections[i] === conn){
-            connections.splice(i,1)
-            i--;
-          }
-        }
-      })
-    })
-  }
-}
-
-function judgeMessage(message){
-  console.log(message)
-  const str = message.toString().split(" ")
-  // 1. 블록 전부 요청(새로운 유저가 들어 왔을 때)
-  if(str[0] == messageType[0]){
-    if(str[1] !== "" && blockContainer.innerText == ""){
-      blockContainer.innerHTML += '<div>'+str[1]+'<div>'
-    }
-  }
-  // 2. pandding tx 요청(내가 pandding tx 만들어서 나머지에게 보냄)
-  else if(str[0] == messageType[1]){
-    if(str[1] !== ""){
-      txContainer.innerHTML += '<div>'+str[1]+'<div>'
-    }
-  }
-  // 3. pandding tx 가져오기 요청(새로운 유저가 들어와서 여태까지의 pandding tx 요청)
-  else if(str[0] == messageType[2]){
-    if(str[1] !== "" && txContainer.innerText == ""){
-      txContainer.innerHTML += '<div>'+str[1]+'<div>'
-    }
-  }
-  // 4. 블록 생성 요청(내가 블록 만들었을 때)
-  else if(str[0] == messageType[3]){
-    if(str[1] !== ""){
-      blockContainer.innerHTML += '<div>'+str[1]+'<div>'
-    }
-  }
-  else{
-    return 'error'
-  }
-}
-
--- server.js
+++ server.js
@@ -1,48 +1,196 @@
-const { json } = require('express')
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+const express = require("express");
+const app = express();
 
-const users = []
-const socketId = []
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+const { v4: uuidV4, v1: uuidV1 } = require("uuid");
+const fs = require('fs')
+const PORT = process.env.NODE_ENV === "development" ? 3003 : 3013;
+const crypto = require('crypto')
+const users = [];
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  console.log("handle");
+  res.status(200).json({ message: "hi" }).end();
+});
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+// app.get("/:room", (req, res) => {
+//   res.render("room", { roomId: req.params.room });
+// });
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    //console.log(userId)
-    socket.join(roomId)
-    socketId.push(socket.id)
-    users.push(userId)
+//-------------------------------------------------------------------------------------------------
 
-    socket.broadcast.to(roomId).emit('user-connected', userId)
+const potentialbuffer = fs.readFileSync('./GiChul_backup/gichulgenerator-potential-export.json')
+const answerbuffer = fs.readFileSync('./GiChul_backup/gichulgenerator-answer-export.json')
+const potentialJSON = JSON.parse(potentialbuffer)
+const answerJSON = JSON.parse(answerbuffer)
 
-    io.to(socket.id).emit('userId', JSON.stringify(users))
-    //socket.broadcast.to(roomId).emit('userId', JSON.stringify(users))
-    //console.log(JSON.stringify(users))
+const randomNormal = (function sourceRandomNormal(source) {
+  function randomNormal(mu, sigma) {
+    var x, r;
+    mu = mu == null ? 0 : +mu;
+    sigma = sigma == null ? 1 : +sigma;
+    
+    return function() {
+      var y;
+      if (x != null) (y = x), (x = null);
+      else
+        do {
+	  x = source() * 2 - 1;
+	  y = source() * 2 - 1;
+	  r = x * x + y * y;
+	}while(!r || r > 1);
+      return mu + sigma * y * Math.sqrt((-1 * Math.log(r)) / r);
+    };
+  }
+  randomNormal.source = Math.random;
+  return randomNormal;
+})(Math.random);
 
-    socket.on('disconnect', () => {
-      //console.log('disconnect')
-      socket.broadcast.to(roomId).emit('user-disconnected', userId)
-      for(let i = 0; i<users.length; i++){
-        if(users[i] === userId){
-          users.splice(i,1)
-          i--;
-        }
+
+function random_item(items){
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+let ran0 = random_item(Object.keys(potentialJSON))
+let ran1 = random_item(Object.keys(potentialJSON[ran0]))
+let ran2 = random_item(Object.keys(potentialJSON[ran0][ran1]))
+let ran3 = random_item(Object.keys(potentialJSON[ran0][ran1][ran2]))
+
+let buf = potentialJSON[ran0][ran1][ran2][ran3]
+let hardness = 1
+let sigma = 10
+buf = buf.slice(22,31)
+
+function closestRandom(buf){
+  let goal = Math.floor(randomNormal(100 - hardness, sigma)())
+  const closest = buf.reduce((prev, curr) => {
+    return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr: prev);
+  });
+  return buf.indexOf(closest) + 22
+}
+
+let pickindex = closestRandom(buf)
+
+
+let imgpath = "./problem/"+ran0+"_"+ran2+"_"+ran1+"_"+ran3+"/q_"+ran0+"_"+ran2+"_"+ran1+"_"+ran3+"_"+pickindex+".png"
+//console.log("path : ", imgpath)
+let answer = answerJSON[ran0][ran1][ran2][ran3][pickindex]
+//console.log("answer : ", answer)
+
+function changeHardness(num){
+  hardness = Math.atan(num)
+}
+
+//-----------------------------------------------------------------------------
+let beforeProblem_id = "";
+let beforeHash = "";
+let currentProblem_id = "";
+let currentHash = "";
+let contents = "";
+let userAnswerSocket = []
+
+//현재 문재에 대한 정답자 수
+let rightCounts= 0;
+
+const currentRoomId = "currentRoom" ?? uuidV4();
+io.on("connect", (socket) => {
+  socket.emit("roomId", currentRoomId);
+
+  socket.on("join-room", (roomId, userId) => {
+    console.log("on join-room", roomId, userId);
+    socket.join(roomId);
+
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+    socket.emit("user-list", {users, beforeHash});
+    
+    socket.emit("NEW_PROBLEM", contents);
+
+    users.push(userId);
+    changeHardness(users.length)
+
+    console.log("users", users);
+
+    socket.on("disconnect", () => {
+      console.log(userId, "is disconnected");
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+      if (users.indexOf(userId) !== -1) {
+        users.splice(users.indexOf(userId), 1);
+        changeHardness(users.length)
       }
-    })
-  })
-})
+    });
+  });
 
-server.listen(3000)
+    
+  socket.on('ANSWER', clientAnswer=>{
+    if(!userAnswerSocket.find(v => v == socket.id)){
+      let msg = currentProblem_id + clientAnswer
+      let tmp = crypto.createHash('sha256').update(msg).digest('base64')
+     
+      socket.emit("USER_HASH", tmp)
+      userAnswerSocket.push(socket.id)
+
+      if(tmp === currentHash){
+        rightCounts++;
+      }
+    }
+  })  
+});
+
+let count = 0
+  
+setInterval(()=>{
+  if(count > 0){
+    io.emit('REAL_HASH', currentHash)
+  }
+  userAnswerSocket = []
+  ran0 = random_item(Object.keys(potentialJSON))
+  ran1 = random_item(Object.keys(potentialJSON[ran0]))
+  ran2 = random_item(Object.keys(potentialJSON[ran0][ran1]))
+  ran3 = random_item(Object.keys(potentialJSON[ran0][ran1][ran2]))
+     
+  buf = potentialJSON[ran0][ran1][ran2][ran3]
+  buf = buf.slice(22,31)
+  pickindex = closestRandom(buf)
+  imgpath = "./GiChul_backup/"+ran0+"_"+ran2+"_"+ran1+"_"+ran3+"/q_"+ran0+"_"+ran2+"_"+ran1+"_"+ran3+"_"+pickindex+".png";
+  console.log(imgpath)
+
+  answer = answerJSON[ran0][ran1][ran2][ran3][pickindex]
+  answer = 1
+  contents = fs.readFileSync(imgpath, {encoding: 'base64'})
+ 
+  if(rightCounts>= 1){
+    beforeProblem_id = currentProblem_id;
+    beforeHash = currentHash;
+    rightCounts= 0;
+  }
+  console.log('beforeHash : '+ beforeHash);
+  currentProblem_id = imgpath + uuidV1();
+  currentHash = crypto.createHash('sha256').update(currentProblem_id + answer).digest('base64')
+
+  //console.log("beforeProblemId : " + beforeProblem_Id)
+  //console.log("beforeHash : " + beforeHash)
+  io.emit('NEW_PROBLEM', contents)
+  count++
+}, 1000 * 60);
+
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  proxied: true,
+});
+peerServer.on("connection", () => console.log("peerServer is conencted"));
+app.use("/peerjs", peerServer);
+
+const cors = require("cors");
+app.use(cors());
+
+server.listen(PORT);
